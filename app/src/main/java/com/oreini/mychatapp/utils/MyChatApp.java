6794cff1d40b80b527ab68ee50e544805f277371
Or Eini <or2eini@gmail.com>
2020-12-16 00:11:16 +0200

    Application class added

---
+++ app/src/main/java/com/oreini/mychatapp/utils/MyChatApp.java
@@ -0,0 +1,26 @@
+package com.oreini.mychatapp.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.oreini.mychatapp.managers.FirebaseManager;
+import com.oreini.mychatapp.managers.SharedPrefsManager;
+
+public class MyChatApp extends Application {
+
+    public static final String LOG_TAG = "MyChatApp";
+    public static Context mContext;
+    public static SharedPrefsManager mSharedPrefsManager;
+    public static  FirebaseManager mFirebaseManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = getApplicationContext();
+
+        mSharedPrefsManager = SharedPrefsManager.getInstance(mContext);
+        mFirebaseManager = FirebaseManager.getInstance(mContext);
+    }
+
+}
